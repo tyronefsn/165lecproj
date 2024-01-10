1f956d7e19a2c87eba9c15882f965f2d38fd6f29
rusxmat <zyrus.wallit@gmail.com>
2024-01-10 22:40:17 +0800

    fix: video player

--- gui.py
+++ gui.py
@@ -14,32 +14,31 @@ class AppGUI():
         self.appFrame = Frame(root)
         self.appFrame.pack()
 
-        self.video_path = None
-        self.video_capture = None
+        self.video_source = None
+        self.video_capture = cv2.VideoCapture(self.video_source)
 
         self.createVideoMenu()
         self.createVideoArea()
         self.createMenu()
 
+        self.playing = False
+
     def createVideoMenu(self):
-        
         self.videoMenu = Frame(self.appFrame)
         self.videoMenu.pack()
 
-        self.openVideoBtn = Button(self.videoMenu, text="Open Video", font=("Helvetica", 15), height=1, width=6, bg="silver", command=self.openVideo)
+        self.openVideoBtn = Button(self.videoMenu, text="Open Video", font=("Helvetica", 15), height=1, width=10, bg="silver", command=self.openVideo)
         self.openVideoBtn.grid(row=0, column=0)
 
-        self.playVideoBtn = Button(self.videoMenu, text="Play Video", font=("Helvetica", 15), height=1, width=6, bg="silver", command=self.play_video)
+        self.playVideoBtn = Button(self.videoMenu, text="Play", font=("Helvetica", 15), height=1, width=10, bg="silver", command=self.play_video)
         self.playVideoBtn.grid(row=0, column=1)
 
-        self.pauseVideoBtn = Button(self.videoMenu, text="Pause Video", font=("Helvetica", 15), height=1, width=6, bg="silver", command=self.stop_video)
+        self.pauseVideoBtn = Button(self.videoMenu, text="Pause", font=("Helvetica", 15), height=1, width=10, bg="silver", command=self.stop_video)
         self.pauseVideoBtn.grid(row=0, column=2)
-
-
 
     def createVideoArea(self):
         
-        self.videoArea = Canvas(self.appFrame)
+        self.videoArea = Canvas(self.appFrame, width=self.video_capture.get(3), height=self.video_capture.get(4))
         self.videoArea.pack()
 
     def createMenu(self):
@@ -65,31 +64,51 @@ class AppGUI():
         self.setSecond = Spinbox(self.setTimeFrame, from_=0, to=59)
         self.setSecond.grid(row=1, column=2)
 
-        self.setTimeBtn = Button(self.appFrame, text="Set Time", font=("Helvetica", 15), height=1, width=5, bg="silver")
+        self.setTimeBtn = Button(self.appFrame, text="Set Time", font=("Helvetica", 15), height=1, width=10, bg="silver")
         self.setTimeBtn.pack()
 
     def play_video(self):
+        net = cv2.dnn.readNet("data/yolov4.weights", "data/yolov4.cfg")
+        layer_names = net.getUnconnectedOutLayersNames()
+
+        # Load COCO class names
+        with open("data/coco.names", "r") as f:
+            classes = [line.strip() for line in f.readlines()]
+
+        # Initialize webcam
+        file_path = filedialog.askopenfilename(filetypes=[("MP4 files", "*.mp4")])
+        cap = cv2.VideoCapture(file_path)
+        fps_start_time = datetime.datetime.now()
+        fps = 0
+        total_frames = 0
+        # tracker = CentroidTracker(maxDisappeared=80, maxDistance=90)
+        tracker = CentroidTracker(maxDisappeared=80, maxDistance=90)
+        prev_box = []
+        prev_conf = []
+
+
+        self.playing = True
         if self.video_capture:
-            while True:
+            self.playing = True
+            while self.playing:
                 ret, frame = self.video_capture.read()
                 if not ret:
                     break
 
                 self.display_frame(frame)
-                self.root.update_idletasks()
-                self.root.update()
+                self.appFrame.update_idletasks()
+                self.appFrame.update()
 
     def stop_video(self):
-        if self.video_capture:
-            self.video_capture.release()
+        self.playing = False
 
 
     def openVideo(self):
         file_path = filedialog.askopenfilename(initialdir='../', title="Select Video File", filetypes=(("Video Files", "*.mp4"), ("all files", "*.*")))
 
         if file_path:
-            self.video_path = file_path
-            self.video_capture = cv2.VideoCapture(self.video_path)
+            self.video_capture.release()
+            self.video_capture = cv2.VideoCapture(file_path)
 
             ret, frame = self.video_capture.read()
             if ret:
